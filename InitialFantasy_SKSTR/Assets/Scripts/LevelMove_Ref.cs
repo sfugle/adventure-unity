149481fcf9b396e0becc97e4226b0eba9bbd9841
Tiara Allard <allard.ti@gmail.com>
2024-04-14 16:22:50 -0400

    house and map work

---
+++ InitialFantasy_SKSTR/Assets/Scripts/LevelMove_Ref.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMove_Ref : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    void Awake()
+    {
+        //Make Collider2D as trigger 
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void onTriggerEnter2D(Collider2D other) {
+        Debug.Log("trigger touched");
+        if(other.CompareTag("Player")) {
+            SceneManager.LoadScene("Map");
+        }
+    }
+}
